512eea6841d76fa3cc2476b784b3b0e551b38dbe
ingolager <ingvarla@gmail.com>
2019-09-07 23:06:07 +0100

    update with report page etcetera

--- src/Report.js
+++ src/Report.js
@@ -1,47 +1,14 @@
 import React, { Component } from 'react';
-import { description } from './Me.json';
+import { reporthead, reporttext } from './Me.json';
 
 class Report extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            questions: [],
-            kmom: props.match.params.kmom
-        };
-    }
-
-  componentDidMount() {
-      let that = this;
-      fetch('./Me.json')
-      .then(function(response) {
-        return response.json();
-      })
-      .then(function(result) {
-          that.setState({
-              questions: result.data
-          });
-      });
-  }
-
   render() {
-    const renderedQuestions = this.state.questions.map((question, index) => {
-        return (
-            <div className="question" key={index}>
-                <p><strong>{ question.question }</strong></p>
-                <p>{ question.answer }</p>
-                <p>{ description }</p>
-            </div>
-        )
-    });
-
-    return (
-        <main>
-            <h2>{ this.state.kmom }</h2>
-            <p>{ description }</p>
-            { renderedQuestions }
-        </main>
-
-
+      return (
+          <div className="Me">
+            <h2> { reporthead} </h2>
+            <p> { reporttext } </p>
+            <p> <a href="https://github.com/ingolager/jsramverk-react">Appens GitHub-repo</a> </p>
+          </div>
     );
   }
 }

18837cbed5ab182464da747b2a2379c67787f9db
ingolager <ingvarla@gmail.com>
2019-10-05 21:00:40 +0100

    backend processing

---
+++ app.js
@@ -0,0 +1,74 @@
+const express = require("express");
+const cors = require('cors');
+const morgan = require('morgan');
+const app = express();
+const port = 1337;
+const bodyParser = require("body-parser");
+const index = require('./routes/index');
+const hello = require('./routes/hello');
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('./db/texts.sqlite');
+
+db.run("INSERT INTO users (email, password) VALUES (?, ?)",
+    "user@example.com",
+    "superlonghashedpasswordthatwewillseehowtohashinthenextsection", (err) => {
+    if (err) {
+        // returnera error
+    }
+
+    // returnera korrekt svar
+});
+
+app.use(cors());
+
+// don't show the log when it is test
+if (process.env.NODE_ENV !== 'test') {
+    // use morgan to log at command line
+    app.use(morgan('combined')); // 'combined' outputs the Apache style LOGs
+}
+
+// This is middleware called for all routes.
+// Middleware takes three parameters.
+app.use((req, res, next) => {
+    console.log(req.method);
+    console.log(req.path);
+    next();
+});
+
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+
+app.use('/', index);
+app.use('/hello', hello);
+
+
+
+// Add routes for 404 and error handling
+// Catch 404 and forward to error handler
+// Put this last
+app.use((req, res, next) => {
+    var err = new Error("Not Found");
+    err.status = 404;
+    next(err);
+});
+
+app.use((err, req, res, next) => {
+    if (res.headersSent) {
+        return next(err);
+    }
+
+    res.status(err.status || 500).json({
+        "errors": [
+            {
+                "status": err.status,
+                "title":  err.message,
+                "detail": err.message
+            }
+        ]
+    });
+});
+
+
+// Start up server
+app.listen(port, () => console.log(`Example API listening on port ${port}!`));

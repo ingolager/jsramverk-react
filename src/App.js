564a6c23778dd7c65c0d5ece7f37ad54369d8e93
ingolager <ingvarla@gmail.com>
2019-10-19 16:53:33 +0100

    corrections

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import './App.css';
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
--- src/Me.js
+++ src/Me.js
@@ -2,6 +2,18 @@ import React, { Component } from 'react';
 import { meHead, meText } from './Me.json';
 import jag from './jag.jpg'
 
+// class Me extends Component {
+//   render() {
+//       return (
+//           <div className="Me">
+//             <h2> { meHead } </h2>
+//             <img src={jag} alt="jag" />
+//             <p> { meText } </p>
+//           </div>
+//     );
+//   }
+// }
+
 class Me extends Component {
   render() {
       return (

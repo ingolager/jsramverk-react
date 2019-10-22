fd95751095ceb406569b16b5723198c3583e9d1a
ingolager <ingvarla@gmail.com>
2019-10-22 21:17:57 +0100

    update test

--- src/Me.js
+++ src/Me.js
@@ -1,26 +1,33 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import { meHead, meText } from './Me.json';
 import jag from './jag.jpg'
 
-// class Me extends Component {
-//   render() {
-//       return (
-//           <div className="Me">
-//             <h2> { meHead } </h2>
-//             <img src={jag} alt="jag" />
-//             <p> { meText } </p>
-//           </div>
-//     );
-//   }
-// }
 
 class Me extends Component {
+    constructor(props) {
+    super(props);
+    this.state = { apiResponse: [] };
+}
+
+componentDidMount() {
+    // axios.get('http://localhost:1337/me')
+    axios.get('https://backend.ingolager.me/me')
+        .then(res => {
+            const apiResponse = res.data;
+            this.setState({ apiResponse });
+            const response = this.state.apiResponse.data.msg;
+            this.setState({response})
+            console.log(response);
+    })
+}
+
   render() {
       return (
           <div className="Me">
             <h2> { meHead } </h2>
             <img src={jag} alt="jag" />
-            <p> { meText } </p>
+            <p> { this.state.response } </p>
           </div>
     );
   }
